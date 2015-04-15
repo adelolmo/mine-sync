e83f3b51ed3ff9dc8f7875bdbc93a77998781e21
andoni <andoni.delolmo@gmail.com>
2015-04-15 22:08:49 +0200

    cleanup release process.

--- release.sh
+++ release.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-RELEASE_VERSION=$1
-DEVELOPMENT_VERSION=$2
+DEVELOPMENT_VERSION=$1
 
 MVN=/opt/maven/apache-maven-3.1.1/bin/mvn
 
 usage(){
-	echo "Usage: $0 release_version development_version"
+	echo "Usage: $0 next_development_version"
+	echo
 }
 
 run(){
+	RELEASE_VERSION=`grep -oP '(?<=<version>).*(?=</version>)' pom.xml|sed -n "1p"|grep -oE "[0-9\.]+"`
+	echo "$RELEASE_VERSION"
 	$MVN versions:set -DnewVersion="$RELEASE_VERSION" &&
 	git commit -a -m "prepare to release v.$RELEASE_VERSION" &&
 	git push &&
@@ -20,7 +22,7 @@ run(){
 	find -name "*.versionsBackup"| xargs -I file rm file && find -name "*.releaseBackup"| xargs -I file rm file
 }
 
-if [[ -n "$RELEASE_VERSION" && -n "$DEVELOPMENT_VERSION" ]]; then
+if [[ -n "$DEVELOPMENT_VERSION" ]]; then
 	run
 else
 	usage
